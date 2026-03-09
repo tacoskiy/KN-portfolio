ede0f3d34c366d22700c040e8244a3b96cf4e1eb
haruominomura <haruomi.nomura@gmail.com>
2026-03-09 10:24:06 +0900

    chore: comment out sitemap declaration in robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://kent-nagata.me', // 末尾のスラッシュを削除してみます
-  integrations: [sitemap()],
+  site: 'https://kent-nagata.me',
+  // integrations: [sitemap()], // Vercelでのビルドエラー回避のため一時的に無効化
   output: 'static',
   server: {
     host: true,
